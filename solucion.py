988b4d268216afeba3e1921270a8a7d80bac1826
Cristian Reyes <ccreyesmolina@gmail.com>
2023-09-19 22:46:30 -0500

    add bubble sort algorithm

--- solucion.py
+++ solucion.py
@@ -1,15 +1,21 @@
 with open('./inputs/Entrada-800.txt', 'r') as file:
 	lines = file.readlines()
-	input800 = [line.strip() for line in lines]
+	input800 = [int(line.strip()) for line in lines]
 
 with open('./inputs/Entrada-8000.txt', 'r') as file:
 	lines = file.readlines()
-	input8000 = [line.strip() for line in lines]
+	input8000 = [int(line.strip()) for line in lines]
 
 with open('./inputs/Entrada-80000.txt', 'r') as file:
 	lines = file.readlines()
-	input80000 = [line.strip() for line in lines]
+	input80000 = [int(line.strip()) for line in lines]
 
-print(input800)
-print(input8000)
-print(input80000)
+import time
+
+def bubble(input : list[int])-> {list[int],int}:
+	now = time.time()
+	for i in range(len(input)):
+		for j in range(len(input)-1):
+			if input[j] > input[j+1]:
+				input[j], input[j+1] = input[j+1], input[j]
+	return {'sortedList': input, 'time': time.time() - now}
