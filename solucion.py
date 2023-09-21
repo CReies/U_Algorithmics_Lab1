1cd2f06f6712f47783f0f07fcaad250730f57bd1
Cristian Reyes <ccreyesmolina@gmail.com>
2023-09-21 15:35:55 -0500

    delete the time counting funcitionality

--- solucion.py
+++ solucion.py
@@ -14,28 +14,25 @@ with open('./inputs/Entrada-80000.txt', 'r') as file:
 
 
 # The bubble method consist in a for loop inside another for loop, the inside loop is where the operations are made, it compares the current element with the next one, if the current element is bigger than the next one, it swaps them, and it does this until the list is sorted.
-def bubble(input : list[int])-> {list[int],int}:
-	now = time.time()
+def bubble(input : list[int])-> list[int]:
 	for i in range(len(input)):
 		for j in range(len(input)-1):
 			if input[j] > input[j+1]:
 				input[j], input[j+1] = input[j+1], input[j]
-	return {'sortedList': input, 'time': time.time() - now}
+	return input
 
 # The selection method consist in a for loop inside another for loop, it finds the minimum element in the list and swaps it with the first element, then it finds the second minimum element and swaps it with the second element, and it does this until the list is sorted.
-def selection(input : list[int])-> {list[int],int}:
-	now = time.time()
+def selection(input : list[int])-> list[int]:
 	for i in range(len(input)):
 		min = i
 		for j in range(i+1, len(input)):
 			if input[min] > input[j]:
 				min = j
 		input[i], input[min] = input[min], input[i]
-	return {'sortedList': input, 'time': time.time() - now}
+	return input
 
 # The insertion method consist in 2 loops, the first one is a for loop that starts in the second element of the list, the second one is a while loop that compares the current element with the previous one, if the current element is smaller than the previous one, it swaps them, then compares it with the second previous element, and if is smaller, it swaps them, and it does this until the current element is bigger than the previous one, then it goes to the next element and does the same until the list is sorted.
-def insertion(input : list[int])-> {list[int],int}:
-	now = time.time()
+def insertion(input : list[int])-> list[int]:
 	for i in range(1, len(input)):
 		key = input[i]
 		j = i - 1
@@ -43,13 +40,10 @@ def insertion(input : list[int])-> {list[int],int}:
 			input[j + 1] = input[j]
 			j -= 1
 		input[j + 1] = key
-	return {'sortedList': input, 'time': time.time() - now}
+	return input
 
 # The merge method consist in a recursive function that divides the list in 2 parts, then it calls itself with the left part and the right part, and it does this until the list is divided in single elements, then it starts to merge the lists, it compares the first element of the left list with the first element of the right list, if the first element of the left list is smaller, it adds it to the final list, and it does this until one of the lists is empty, then it adds the remaining elements of the other list to the final list, and it does this until the whole list is sorted.
-def merge(input : list[int] or {list[int],int})-> {list[int],int}:
-	if isinstance(input, dict):
-		input = input['sortedList']
-	now = time.time()
+def merge(input : list[int])-> list[int]:
 	if len(input) > 1:
 		mid = len(input) // 2
 		left = input[:mid]
